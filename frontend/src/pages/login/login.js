ccb6af4389b0b014220736585368c4778ce3501e
Jonathan santos <jonathandossantos.c@gmail.com>
2019-08-11 21:21:45 -0300

    feat: list, like and dislike

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,15 +1,35 @@
-import React from 'react'
-
-import logo from '../../assets/logo.svg'
+import React, {useState} from 'react'
 import './login.scss'
 
-export default function Login() {
+import logo from '../../assets/logo.svg'
+
+import api from '../../services/api'
+
+export default function Login( {history}) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit (e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username
+        })
+
+        const { _id } = response.data
+
+        history.push(`/dev/${_id}`)
+    }
+
     return (
         <div className="login">
             <div className="login__container">
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <img src={logo} alt="tindev" />
-                    <input placeholder="Digite seu usuário no Github" type="text" />
+                    <input 
+                        placeholder="Digite seu usuário no Github" 
+                        type="text" 
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}/>
                     <button type="submit">Enviar</button>
                 </form>
             </div>
